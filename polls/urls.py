52f74ffc10d4f59d3b0eea4a39746e72286cddbd
coding-fantastic <alexnzioka@protonmail.com>
2020-05-21 22:20:48 +0300

    page for the urls of the functions in views

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
+
 from . import views
 urlpatterns = [
-    path('',views.index, name='index')
+    #call index function from views
+    # execute /polls/
+    path('',views.index, name='index'),
+    # /polls/5/
+    path('<int:question_id',views.detail, name='detail'),
+    # call results function from views file
+    # /polls/5/results
+    path('<int:question_id/results',views.results, name='results'),
+    # call vote function from views file
+    # /polls/5/vote
+    path('<int:question_id/vote',views.vote, name='vote'),
 ]
